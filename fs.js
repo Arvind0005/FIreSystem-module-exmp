6507401fb7d4d4592d392c865f2ebb8e3753ef33
Arvind_Suresh <53540697+Arvind0515@users.noreply.github.com>
2021-07-24 00:54:28 +0530

    cganges

--- fs.js
+++ fs.js
@@ -1,25 +1,27 @@
 var fs = require('fs');
 
+///to create a file.
 // var createStream = fs.createWriteStream('JournalDev.txt');
 // createStream.end();
 
-
+///to write a file.
 // var createwriteStream = fs.createWriteStream('JournalDev.txt');
 // createwriteStream.write("Hello World");
 // createwriteStream.write("\nok bye");
 // createwriteStream.end(); 
 
-// function readdata(err,data)
-// {
-//     console.log(data);
-//     if(err)
-//     {
-//         console.log(err);
-//     }
-// }
 
-fs.readFile('JournalDev.txt',function readdata(error,data)
+//to readdata from the file.
+function readdata(err,data)
 {
     console.log(data);
+    if(err)
+    {
+        console.log(err);
+    }
 }
-);
+
+//readdata();
+
+//to readdata from the file.
+fs.readFile('JournalDev.txt','utf8',readdata);
